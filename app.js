2ae8f3065570d6342921cc2cb2d922458d1acfca
Hikaru Watanabe <hwatanabe@diverta.co.jp>
2022-11-09 16:00:07 +0900

    Install some plugins

--- app.js
+++ app.js
@@ -3,11 +3,49 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport';
 
 ClassicEditor
     .create( document.querySelector( '#editor' ), {
-        plugins: [ Essentials, Paragraph, Bold, Italic ],
-        toolbar: [ 'bold', 'italic' ]
+        plugins: [
+            Essentials,
+            Paragraph,
+            Bold,
+            Italic,
+            SourceEditing,
+            Image,
+            ImageToolbar,
+            ImageResize,
+            GeneralHtmlSupport,
+        ],
+        toolbar: [
+            'sourceEditing',
+            '|',
+            'bold',
+            'italic',
+        ],
+        image: {
+            toolbar: [
+                'imageResize',
+            ],
+            insert: {
+                type: 'block',
+            },
+        },
+        htmlSupport: {
+            allow: [
+                {
+                    name: /.*/,
+                    attributes: true,
+                    classes: true,
+                    styles: true
+                }
+            ]
+        },
     } )
     .then( editor => {
         console.log( 'Editor was initialized', editor );
